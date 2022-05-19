561f45e0a1a0f4459a3b179e309f85208b15a348
jp-quintana <juanquintana1996@gmail.com>
2022-05-19 00:18:48 -0300

    Termine seccion 20

---
+++ react-cg-section-20/project-v6-ejemplo-2/src/pages/Welcome.js
@@ -0,0 +1,15 @@
+import { Link, Outlet } from 'react-router-dom';
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Link to="new-user">
+        New User
+      </Link>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Welcome;
